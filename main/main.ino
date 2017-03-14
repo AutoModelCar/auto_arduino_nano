ea2cb1533b372067aa0b4f23b668ea917563fd61
zahraboroujeni <zahra.boroujeni@gmail.com>
2017-03-14 17:03:15 +0100

    initilize the IMU using IMU_Zero

--- main/main.ino
+++ main/main.ino
@@ -200,10 +200,12 @@ void setup() {
     Serial.println(F("Initializing DMP..."));
     devStatus = mpu.dmpInitialize();
     // supply your own gyro offsets here, scaled for min sensitivity
-    mpu.setXGyroOffset(220);
-    mpu.setYGyroOffset(76);
-    mpu.setZGyroOffset(-85);
-    mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
+    mpu.setXGyroOffset(104);
+    mpu.setYGyroOffset(-6);
+    mpu.setZGyroOffset(17);
+    mpu.setZAccelOffset(2007); // 1688 factory default for my test chip
+    mpu.setYAccelOffset(-425);
+    mpu.setXAccelOffset(-3060);
     // make sure it worked (returns 0 if so)
     if (devStatus == 0) {
         // turn on the DMP, now that it's ready
